003911e94ca12abdef41d02e450e52ba15ab0fc6
Alya-wife <ravywhienelda@gmail.com>
2026-07-21 22:10:47 +0700

    Ganti GASURL
    Updated GAS_URL to a new Google Apps Script endpoint.

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     // Google Apps Script URL - isi setelah deploy GAS (Publish -> Deploy as Web App)
-    GAS_URL: 'https://script.google.com/macros/s/AKfycbw65cMdO0LvfHMctn6DGifEHtcYwFD_f2Gs1ajfJ9sHg4yfhR1JnuaLYoObjeMgjRQDYA/exec', // GANTI INI NANTI
+    GAS_URL: 'https://script.google.com/macros/s/AKfycbyrt1F3fTh0R-ozV4WWDGPtdGPI2lSKB9dwLjAjYD_yR0TPyI8Y4pAmjPu1BlKTIpenkg/exec', // GANTI INI NANTI
 
     // Google Sheets IDs (dari spesifikasi)
     SHEET_LOGIN_ID: '1Q8BJBbL-Rk0QEq5HeroP4AMM4G3pmI1qW3i9RC7qrvs',
