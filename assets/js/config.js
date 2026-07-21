f344e874442edd18b4ac988e15b8205cd2f41429
Alya-wife <ravywhienelda@gmail.com>
2026-07-21 22:50:23 +0700

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     // Google Apps Script URL - isi setelah deploy GAS (Publish -> Deploy as Web App)
-    GAS_URL: 'https://script.google.com/macros/s/AKfycbxNjQpBbntyTQCR4ZU2wmvHW_Ijnz0nBqZShSOMgbcloTSrEObFTtTvzNsw-daZ3uyh_g/exec', // GANTI INI NANTI
+    GAS_URL: 'https://script.google.com/macros/s/AKfycbxd_vUTTuu3_48Z2BeapLDeEqRRb1pgrVs3aQXPxvfx7dSfJi2AwlsjV24nLMAmrsvZIw/exec', // GANTI INI NANTI
 
     // Google Sheets IDs (dari spesifikasi)
     SHEET_LOGIN_ID: '1Q8BJBbL-Rk0QEq5HeroP4AMM4G3pmI1qW3i9RC7qrvs',
