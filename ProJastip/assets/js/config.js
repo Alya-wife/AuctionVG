cf51bb3dbd126268d40543d8bce390133c738024
Alya-wife <ravywhienelda@gmail.com>
2026-07-21 21:19:27 +0700

    Add files via upload

---
+++ ProJastip/assets/js/config.js
@@ -0,0 +1,14 @@
+const CONFIG = {
+    // Google Apps Script URL - isi setelah deploy GAS (Publish -> Deploy as Web App)
+    GAS_URL: 'https://script.google.com/macros/s/AKfycbw65cMdO0LvfHMctn6DGifEHtcYwFD_f2Gs1ajfJ9sHg4yfhR1JnuaLYoObjeMgjRQDYA/exec', // GANTI INI NANTI
+
+    // Google Sheets IDs (dari spesifikasi)
+    SHEET_LOGIN_ID: '1Q8BJBbL-Rk0QEq5HeroP4AMM4G3pmI1qW3i9RC7qrvs',
+    SHEET_DATA_ID: '1CcD1CAkXkus0iHLg4Q07RutGx85nsaB6-hBGPU-qGKI',
+
+    // Google Drive folder untuk gambar
+    DRIVE_FOLDER_ID: '1liegX11st8HsJ5KNAC_gVfHxslThH9N0',
+
+    // Mode: true = pakai LocalStorage (localhost), false = pakai GAS
+    USE_LOCAL: false
+};
