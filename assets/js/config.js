22af116276086ce1f613bcb7e5915aaf3d5f39b3
Alya-wife <ravywhienelda@gmail.com>
2026-07-27 18:12:11 +0700

    Update GAS_URL in config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     // Google Apps Script URL
-    GAS_URL: 'https://script.google.com/macros/s/AKfycbw65cMdO0LvfHMctn6DGifEHtcYwFD_f2Gs1ajfJ9sHg4yfhR1JnuaLYoObjeMgjRQDYA/exec',
+    GAS_URL: 'https://script.google.com/macros/s/AKfycbxGMYIolMrDAL8I_kDdLAWTCBfiLO8MARVHFH42n1rykPT0wNradR2CoAij9yPc6Wg2SA/exec',
 
     // Google Sheets IDs
     SHEET_LOGIN_ID: '1Q8BJBbL-Rk0QEq5HeroP4AMM4G3pmI1qW3i9RC7qrvs',
