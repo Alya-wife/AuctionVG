9141887d4854e2db61b64f17529e742e85045f65
Alya-wife <ravywhienelda@gmail.com>
2026-07-28 10:52:07 +0700

    Add files via upload

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     // Google Apps Script URL
-    GAS_URL: 'https://script.google.com/macros/s/AKfycbw65cMdO0LvfHMctn6DGifEHtcYwFD_f2Gs1ajfJ9sHg4yfhR1JnuaLYoObjeMgjRQDYA/exec',
+    GAS_URL: 'https://script.google.com/macros/s/AKfycbwFvDcCtr5YNy3e4O_c6dl4ruKKBRsfqZRd4R7MEgbTO--EUMALgfP0n8iApTgaJWfn2A/exec',
 
     // Google Sheets IDs
     SHEET_LOGIN_ID: '1Q8BJBbL-Rk0QEq5HeroP4AMM4G3pmI1qW3i9RC7qrvs',
